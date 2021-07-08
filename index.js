457c8796bc00a5663205adb319e3e1e3084d0631
Myllena Cruz <myllena12345@hotmail.com>
2021-07-08 17:02:07 -0300

    Base e organização

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const connection = require('./database/database')
+
+const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Tudo ok!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+app.use('/', categoriesController)
+app.use('/', articlesController)
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(9898, () => {
+  console.log('Rodando!')
+})
