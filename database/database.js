e8c107874cc76067d490c31d2f8907ef40d84264
Myllena Cruz <myllena12345@hotmail.com>
2021-07-08 22:55:03 -0300

    Artigos e tabelas

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const connection = new Sequelize('admpress', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '-03:00',
 })
 
 module.exports = connection
