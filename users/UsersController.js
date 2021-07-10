19cdf6eaa1b15e7742cbb7c98e486ca594872d0c
Myllena Cruz <myllena12345@hotmail.com>
2021-07-10 16:13:11 -0300

    Hash de senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('./User')
+const bcrypt = require('bcryptjs')
 
 router.get('/admin/users', (req, res) => {
   res.send('Usuários:')
@@ -13,7 +14,20 @@ router.get('/admin/users/create', (req, res) => {
 router.post('/users/create', (req, res) => {
   let email = req.body.email
   let password = req.body.password
-  res.json({ email, password })
+
+  let salt = bcrypt.genSaltSync(10)
+  let hash = bcrypt.hashSync(password, salt)
+
+  User.create({
+    email: email,
+    password: hash
+  })
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(() => {
+      res.redirect('/')
+    })
 })
 
 module.exports = router
