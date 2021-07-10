0f8e34ff59395877c62fe7838ec0c9a7c23009f1
Myllena Cruz <myllena12345@hotmail.com>
2021-07-10 15:17:07 -0300

    Base do sistema de login

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const connection = require('./database/database')
 
 const categoriesController = require('./categories/CategoriesController')
 const articlesController = require('./articles/ArticlesController')
+const usersController = require('./users/UsersController')
 
 const Article = require('./articles/Article')
 const Category = require('./categories/Category')
+const User = require('./users/User')
 
 app.set('view engine', 'ejs')
 
@@ -26,6 +28,7 @@ connection
 
 app.use('/', categoriesController)
 app.use('/', articlesController)
+app.use('/', usersController)
 
 app.get('/', (req, res) => {
   Article.findAll({
