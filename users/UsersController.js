6d6aa18781ecc8f8bb73ada8a49633ffd259ead2
Myllena Cruz <myllena12345@hotmail.com>
2021-07-11 18:41:14 -0300

    Autorização + middleware

--- users/UsersController.js
+++ users/UsersController.js
@@ -54,7 +54,7 @@ router.post('/authenticate', (req, res) => {
           id: user.id,
           email: user.email,
         }
-        res.json(req.session.user)
+        res.redirect('/admin/articles')
       } else {
         res.redirect('/login')
       }
